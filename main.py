9284d6ee43916b50118b667b2b97a25d200ee976
tawanharink <99250038+tawanharink@users.noreply.github.com>
2023-05-21 11:24:31 +0200

    Database opgezet en Login page gemaakt

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
-import curses
 from menu import Menu
+import sqlite3
 
+# database creation
+con = sqlite3.connect('fitnessplus.db')
+cur = con.cursor()
+
+# role 1 = super admin, 2 = system admin, 3 = trainer
+cur.execute('''CREATE TABLE IF NOT EXISTS users 
+            (username text PRIMARY KEY, password text, firstname text, lastname text, email text, registration_date text, role number)''')
+
+cur.execute('''INSERT OR IGNORE INTO users VALUES
+            ('super_admin', 'Admin_123!', :null, :null, :null, :null, 1)''', 
+            {"null": None})
+
+cur.execute('''CREATE TABLE IF NOT EXISTS members 
+            (id integer PRIMARY KEY, firstname text, lastname text, address text, email text, phone integer)''')
+
+con.commit()
 
 def main():
-    login_menu = Menu([("Username", None), ("Password", None)], curses.initscr(), "Login Menu")
-
-    m = Menu([("Login", login_menu), ("Register", None)], curses.initscr(), "Main Menu")
-    m.run()
+    menu = Menu(["Login"])
+    menu.display()
+    choice = menu.get_user_choice()
+    if choice == 1:
+        menu.login()
+    else:
+        print("Invalid choice.")
 
 
 if __name__ == "__main__":
     main()
+
+
--- menu.py
+++ menu.py
@@ -1,61 +1,45 @@
-import curses
+import sqlite3
+import os
 
+def clear():
+    os.system('cls' if os.name=='nt' else 'clear')
 
 class Menu:
-    def __init__(self, items, stdscreen, title):
-        self.current_row_idx = None
-        self.title = title
-        self.window = stdscreen.subwin(0, 0)
-        self.window.keypad(1)
-        self.items = items
-        self.items.append(('exit', 'exit'))
+    def __init__(self, options):
+        self.options = options
 
-    def display_menu(self):
-        self.window.clear()
-        self.window.border(0)
-        self.window.addstr(2, 2, self.title)
-        if self.title == "Main Menu":
-            self.window.addstr(4, 2, 'Use arrow keys to navigate')
-            self.window.addstr(5, 2, 'Press Enter to select an option')
-
-        # Display menu items
-        for index, item in enumerate(self.items):
-            if index == self.current_row_idx:
-                mode = curses.A_REVERSE
-            else:
-                mode = curses.A_NORMAL
-
-            msg = '%d. %s' % (index, item[0])
-            self.window.addstr(6 + index, 4, msg, mode)
-
-        self.window.refresh()
-
-    def run(self):
-        self.current_row_idx = 0
-
-        # Loop until user selects a menu item
+    def display(self):
+        print("Menu:")
+        for index, option in enumerate(self.options, start=1):
+            print(f"[{index}] {option}")
+        
+    def get_user_choice(self):
         while True:
-            self.display_menu()
-            key = self.window.getch()
-
-            if key in [curses.KEY_ENTER, ord('\n')]:
-                if self.current_row_idx == len(self.items) - 1:
-                    break
+            try:
+                choice = int(input("Enter your choice: "))
+                if 1 <= choice <= len(self.options):
+                    return choice
                 else:
-                    self.items[self.current_row_idx][1].run()
+                    print("Invalid choice. Please try again.")
+            except ValueError:
+                print("Invalid input. Please enter a number.")
 
-            elif key == curses.KEY_UP:
-                self.current_row_idx = (self.current_row_idx - 1) % len(self.items)
+    def login(self):
+        clear()
+        username = input("Enter your username: ")
+        password = input("Enter your password: ")
 
-            elif key == curses.KEY_DOWN:
-                self.current_row_idx = (self.current_row_idx + 1) % len(self.items)
+        # Connect to the SQLite database
+        conn = sqlite3.connect("fitnessplus.db")
+        cursor = conn.cursor()
 
-    def get_input(self):
-        curses.echo()
-        user_input = self.window.getstr(7, 2, 60)
-        curses.noecho()
-        return user_input
+        # Execute a query to check if the username and password combination exists
+        cursor.execute("SELECT * FROM users WHERE username = ? AND password = ?", (username, password))
+        result = cursor.fetchone()
 
+        if result:
+            print("Login successful! You can proceed.")
+        else:
+            print("Invalid username or password. Please try again.")
 
-
-
+        conn.close()
